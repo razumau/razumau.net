575c3957bb036b49e0597958d5d54bcf17f67346
Jury Razumau <jurazumau@gmail.com>
2020-01-03 18:08:02 +0300

    update some parts of bio

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://razumau.net',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
+  title: 'Jury Razumau',
+  subtitle: '',
+  copyright: '',
   disqusShortname: '',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-73379983-2',
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
+      label: 'Posts',
       path: '/'
     },
     {
@@ -25,19 +25,19 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Jury Razumau',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: '',
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
+      email: 'mail@razumau.net',
+      // facebook: '#',
+      // telegram: '#',
+      twitter: 'razumau',
+      github: 'razumau',
+      // rss: '',
+      // vkontakte: '',
+      // linkedin: '#',
+      // instagram: '#',
       line: '',
       gitlab: '',
       weibo: '',
